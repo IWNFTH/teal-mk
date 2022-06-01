ccb0e0bc53029fb33d9bc304c6d87acb8afee02e
IWNFTH <taka2superhard@gmail.com>
2022-06-01 22:01:15 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "opportunities#index"
-  resources :opportunities, only: [:index, :create, :edit, :update] do
+  resources :opportunities, only: [:index, :create, :edit, :update, :show] do
     resources :comments, only: :create
   end
+  resources :users, only: :show
 end
