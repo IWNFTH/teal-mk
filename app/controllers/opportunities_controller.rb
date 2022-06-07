6cd43f9097ddb04183e972239aa7768ef5cf8c6c
IWNFTH <taka2superhard@gmail.com>
2022-06-07 21:28:20 +0900

    CSV出力機能の実装

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,11 +1,19 @@
 class OpportunitiesController < ApplicationController
   before_action :authenticate_user!, only: :new
+  require 'csv'
 
   def index
     @opportunities = Opportunity.all
     @opportunity = Opportunity.new
     @q = Opportunity.ransack(params[:q])
     @opportunities = @q.result
+    # CSV出力機能
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_opportunities_csv(@opportunities)
+      end
+    end
   end
 
   def create
@@ -41,5 +49,27 @@ class OpportunitiesController < ApplicationController
     params.require(:opportunity).permit(:op_name, :company, :person_in_charge, :date_of_obtaining, :date_of_participation, :phase_id,
       :date_of_negotiation, :estimated_sales_amount, :sales_amount, :contracted_date, :lost_order_date, :company_area_id, :free_text, :estimated_approximate_profit, :approximate_profit)
   end
-
+  # CSV出力機能
+  def send_opportunities_csv(opportunities)
+    csv_data = CSV.generate do |csv|
+      column_names = %w(案件名 会社名 エリア 担当者 案件取得日 成約日 予定概算売上 確定概算売上 予定粗利益 確定粗利益)
+      csv << column_names
+      opportunities.each do |op|
+        column_values = [
+          op.op_name,
+          op.company,
+          op.company_area_id,
+          op.date_of_obtaining,
+          op.contracted_date,
+          op.estimated_sales_amount,
+          op.sales_amount,
+          op.estimated_approximate_profit,
+          op.approximate_profit,
+        ]
+        csv << column_values
+      end
+    end
+    send_data(csv_data, filename: "案件一覧.csv")
+  end
+  # /CSV出力機能
 end
